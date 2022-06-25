643c64be86390f266ae8d4f759390b5b76db5cc6
Faheema125 <88572896+Faheema125@users.noreply.github.com>
2022-06-25 08:00:57 +0530

    Update fix_previleges.sh

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod +x /home/ec2-user/spring-petclinic-2.7.0-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ec2-user/server/spring-petclinic-2.7.0-SNAPSHOT.jar
+chmod +x /home/ec2-user/server/start_server.sh
+chmod +x /home/ec2-user/server/stop_server.sh
